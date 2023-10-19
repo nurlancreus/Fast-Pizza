f39d3599c680d39047a849d18eeeac56c9ab23f0
nurlancreus <nurlancreus007@gmail.com>
2023-10-19 18:21:05 +0400

    helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,11 @@
-export function formatCurrency(value) {
+export function formatCurrency(value: number) {
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "EUR",
   }).format(value);
 }
 
-export function formatDate(dateStr) {
+export function formatDate(dateStr: string) {
   return new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "short",
@@ -14,16 +14,16 @@ export function formatDate(dateStr) {
   }).format(new Date(dateStr));
 }
 
-export function calcMinutesLeft(dateStr) {
+export function calcMinutesLeft(dateStr: string) {
   const d1 = new Date().getTime();
   const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
 }
 
-export function firstLetterUpper(word) {
+export function firstLetterUpper(word: string) {
   if (word) {
     const wordArr = word.split("");
-    wordArr.splice(0, 1, wordArr.at(0).toUpperCase());
+    wordArr.splice(0, 1, wordArr[0].toUpperCase());
     return wordArr.join("");
   }
 
